490e0cf1f4a174e8fc9568c8c972c3952a38ec31
alamb93 <alamb93@gmail.com>
2015-06-04 18:00:03 -0400

    Map Started

--- SwordAndScaleTake2/Game1.cs
+++ SwordAndScaleTake2/Game1.cs
@@ -11,12 +11,31 @@ namespace SwordAndScaleTake2
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Terrain[,] map;
+        Texture2D blank;
+        Texture2D yellow;
+        Texture2D mapImage;
+        Texture2D swordImageB;
+        Texture2D mageImageB;
+        Texture2D warriorImageB;
+        Texture2D archerImageB;
+        Texture2D pikeImageB;
+        Texture2D swordImageR;
+        Texture2D mageImageR;
+        Texture2D warriorImageR;
+        Texture2D archerImageR;
+        Texture2D pikeImageR;
+        Vector2 cursorPosition;
+        KeyboardState pressedKey;
+        KeyboardState oldState;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            //Test Commit
+            graphics.PreferredBackBufferHeight = 960;  // set this value to the desired Height of your window
+            graphics.PreferredBackBufferWidth = 1536;  // set this value to the desired width of your window
+            graphics.ApplyChanges();
         }
 
         /// <summary>
@@ -27,7 +46,8 @@ namespace SwordAndScaleTake2
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            loadMap();
+            cursorPosition = new Vector2(0, 0);
 
             base.Initialize();
         }
@@ -40,6 +60,19 @@ namespace SwordAndScaleTake2
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            mapImage = Content.Load<Texture2D>("alphamap");
+            swordImageB = Content.Load<Texture2D>("blueSword");
+            warriorImageB = Content.Load<Texture2D>("blueWarrior");
+            mageImageB = Content.Load<Texture2D>("blueMage");
+            archerImageB = Content.Load<Texture2D>("blueArcher");
+            pikeImageB = Content.Load<Texture2D>("bluePike");
+            swordImageR = Content.Load<Texture2D>("redSword");
+            warriorImageR = Content.Load<Texture2D>("redWarrior");
+            mageImageR = Content.Load<Texture2D>("redMage");
+            archerImageR = Content.Load<Texture2D>("redArcher");
+            pikeImageR = Content.Load<Texture2D>("redPike");
+            blank = Content.Load<Texture2D>("blanks");
+            yellow = Content.Load<Texture2D>("yellow");
 
             // TODO: use this.Content to load your game content here
         }
@@ -62,8 +95,27 @@ namespace SwordAndScaleTake2
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            pressedKey = Keyboard.GetState();
 
-            // TODO: Add your update logic here
+            if (oldState.IsKeyUp(Keys.Left) && pressedKey.IsKeyDown(Keys.Left) && cursorPosition.X > 0)
+            {
+                cursorPosition.X -= 64;
+            }
+
+            if (oldState.IsKeyUp(Keys.Right) && pressedKey.IsKeyDown(Keys.Right) && cursorPosition.X < 23 * 64)
+            {
+                cursorPosition.X += 64;
+            }
+
+            if (oldState.IsKeyUp(Keys.Down) && pressedKey.IsKeyDown(Keys.Down) && cursorPosition.Y < 13 * 64)
+            {
+                cursorPosition.Y += 64;
+            }
+            if (oldState.IsKeyUp(Keys.Up) && pressedKey.IsKeyDown(Keys.Up) && cursorPosition.Y > 0)
+            {
+                cursorPosition.Y -= 64;
+            }
+            oldState = pressedKey;
 
             base.Update(gameTime);
         }
@@ -76,9 +128,30 @@ namespace SwordAndScaleTake2
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
 
+            spriteBatch.Draw(mapImage, new Rectangle(0, 0, 1536, 896), Color.White);
+            spriteBatch.Draw(yellow, cursorPosition, Color.White);
+
+            spriteBatch.End();
             base.Draw(gameTime);
+                        //24*14
+            //64x64 tiles
+            //1536x896
+        }
+
+        public void loadMap()
+        {
+            Terrain square = null;
+            map = new Terrain[24, 14];
+            for (int x = 0; x < 24; x++)
+                for (int y = 0; y < 14; y++)
+                {
+                    {
+                        square = new Terrain(x, y);
+                        map[x, y] = square;
+                    }
+                }
         }
     }
 }
