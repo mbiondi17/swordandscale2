a1a4b1d8906a7b81c0b296ac411928760b242446
alamb93 <alamb93@gmail.com>
2015-06-04 18:00:28 -0400

    Terrain Started

---
+++ SwordAndScaleTake2/Terrain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwordAndScaleTake2
+{
+    class Terrain
+    {
+        int x;
+        int y;
+
+        public Terrain(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
