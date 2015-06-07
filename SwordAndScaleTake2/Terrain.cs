01dc9ceb3ce2b3afc5aacfe412defa8db985f645
mbiondi17 <mvb5bg@virginia.edu>
2015-06-07 18:35:24 -0400

    revert 2

--- SwordAndScaleTake2/Terrain.cs
+++ SwordAndScaleTake2/Terrain.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +13,7 @@ namespace SwordAndScaleTake2
         bool redOccupied;
         bool owner;
         public bool isInteractable;
+        public void Draw();
         Vector2 position;
 
         public Terrain(int x, int y)
@@ -93,7 +92,7 @@ namespace SwordAndScaleTake2
         }
 
         
-        public void Draw(SpriteBatch spriteBatch, Texture2D fire, Texture2D gate, Texture2D redCastle, Texture2D blueCastle, Texture2D poison) 
+        public void Draw() 
         {
 
             if (!this.isInteractable)
@@ -104,14 +103,14 @@ namespace SwordAndScaleTake2
 			        this.getPosition() == new Vector2(6*64, 12*64)  ||
 			        this.getPosition() == new Vector2(9*64, 10*64)  )
 		    {
-			    spriteBatch.Draw(fire, this.getPosition(), Color.White);
+			
 		    }
 
 		       //red livestock
 		        else if( this.getPosition() == new Vector2(5*64, 8*64)  ||
 			        this.getPosition() == new Vector2(1*64, 6*64)       )
 		        {
-                    spriteBatch.Draw(gate, this.getPosition(), Color.White);
+		
 		        }
 
 		        //red fields
@@ -119,7 +118,7 @@ namespace SwordAndScaleTake2
 			        this.getPosition() == new Vector2(1*64, 10*64)      ||
 			        this.getPosition() == new Vector2(4*64, 12*64)      )
 		        {
-                    spriteBatch.Draw(fire, this.getPosition(), Color.White);
+
 		        }
 
 		        //red poisonable river
@@ -130,9 +129,7 @@ namespace SwordAndScaleTake2
 				        this.getPosition() == new Vector2(4*64, 2*64)   ||
 				        this.getPosition() == new Vector2(5*64, 2*64)   )
 		        {
-                    spriteBatch.Draw(poison, new Vector2(3*64, 1*64), Color.White);
-                    spriteBatch.Draw(poison, new Vector2(4 * 64, 1 * 64), Color.White);
-                    spriteBatch.Draw(poison, new Vector2(5 * 64, 1 * 64), Color.White);
+
 		        }
 
 		        //red castle
@@ -141,7 +138,7 @@ namespace SwordAndScaleTake2
 			        this.getPosition() == new Vector2(1*64, 2*64)       ||
 			        this.getPosition() == new Vector2(2*64, 2*64)       )
 		        {
-                    spriteBatch.Draw(blueCastle, new Vector2(64,64), Color.White);
+
 		        }
 
 		        //blue houses
@@ -150,14 +147,14 @@ namespace SwordAndScaleTake2
 			        this.getPosition() == new Vector2(17*64, 6*64)      ||
 			        this.getPosition() == new Vector2(19*64, 8*64)      )
 		        {
-                    spriteBatch.Draw(fire, this.getPosition(), Color.White);
+
 		        }
 
 		        //blue livestock
 		        else if( this.getPosition() == new Vector2(14*64, 3*64) ||
 			        this.getPosition() == new Vector2(22*64, 5*64)      ) 
 		        {
-                    spriteBatch.Draw(gate, this.getPosition(), Color.White);
+
 		        }
 
 		        //blue fields
@@ -165,7 +162,7 @@ namespace SwordAndScaleTake2
 			        this.getPosition() == new Vector2(22*64, 2*64)      ||
 			        this.getPosition() == new Vector2(19*64, 1*64)      )
 		        {
-                    spriteBatch.Draw(fire, this.getPosition(), Color.White);
+
 		        }
 
 		        //blue poisonable river
@@ -175,9 +172,7 @@ namespace SwordAndScaleTake2
 			  	         this.getPosition() == new Vector2(18*64, 11*64)||
 			   	         this.getPosition() == new Vector2(20*64, 11*64) )
 		        {
-                    spriteBatch.Draw(poison, new Vector2(18 * 64, 10 * 64), Color.White);
-                    spriteBatch.Draw(poison, new Vector2(19 * 64, 10 * 64), Color.White);
-                    spriteBatch.Draw(poison, new Vector2(20 * 64, 10 * 64), Color.White);
+
 		        }
 
 		        //blue castle
@@ -186,7 +181,7 @@ namespace SwordAndScaleTake2
 			        this.getPosition() == new Vector2(21*64, 11*64)     ||
 			        this.getPosition() == new Vector2(22*64, 11*64)      )
 		        {
-                    spriteBatch.Draw(redCastle, new Vector2(21, 10), Color.White);
+
 		        }
 
 		        else
